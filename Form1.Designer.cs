a7a4c6e2bfd6055879df0f5a5518b4e0aae7f167
Adonay Russom <90451352+Adonayrussom@users.noreply.github.com>
2022-09-24 12:24:10 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace AddNums
+{
+    partial class frmAddition
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirstNum = new System.Windows.Forms.Label();
+            this.lblSecNum = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.txtSum = new System.Windows.Forms.TextBox();
+            this.txtSecNum = new System.Windows.Forms.TextBox();
+            this.txtFirstNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblFirstNum
+            // 
+            this.lblFirstNum.AutoSize = true;
+            this.lblFirstNum.Location = new System.Drawing.Point(32, 20);
+            this.lblFirstNum.Name = "lblFirstNum";
+            this.lblFirstNum.Size = new System.Drawing.Size(66, 13);
+            this.lblFirstNum.TabIndex = 0;
+            this.lblFirstNum.Text = "First Number";
+            // 
+            // lblSecNum
+            // 
+            this.lblSecNum.AutoSize = true;
+            this.lblSecNum.Location = new System.Drawing.Point(32, 77);
+            this.lblSecNum.Name = "lblSecNum";
+            this.lblSecNum.Size = new System.Drawing.Size(84, 13);
+            this.lblSecNum.TabIndex = 1;
+            this.lblSecNum.Text = "Second Number";
+            this.lblSecNum.Click += new System.EventHandler(this.lblSecNum_Click);
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(32, 131);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(28, 13);
+            this.lblSum.TabIndex = 2;
+            this.lblSum.Text = "Sum";
+            this.lblSum.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnAdd.FlatAppearance.BorderSize = 2;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Location = new System.Drawing.Point(32, 187);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 40;
+            this.btnAdd.Text = "&Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click_1);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnCancel.FlatAppearance.BorderSize = 2;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Location = new System.Drawing.Point(175, 187);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 50;
+            this.btnCancel.Text = "&Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // txtSum
+            // 
+            this.txtSum.Location = new System.Drawing.Point(159, 131);
+            this.txtSum.Name = "txtSum";
+            this.txtSum.Size = new System.Drawing.Size(100, 20);
+            this.txtSum.TabIndex = 30;
+            // 
+            // txtSecNum
+            // 
+            this.txtSecNum.Location = new System.Drawing.Point(159, 77);
+            this.txtSecNum.Name = "txtSecNum";
+            this.txtSecNum.Size = new System.Drawing.Size(100, 20);
+            this.txtSecNum.TabIndex = 24;
+            // 
+            // txtFirstNum
+            // 
+            this.txtFirstNum.Location = new System.Drawing.Point(159, 20);
+            this.txtFirstNum.Name = "txtFirstNum";
+            this.txtFirstNum.Size = new System.Drawing.Size(100, 20);
+            this.txtFirstNum.TabIndex = 10;
+            // 
+            // frmAddition
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.txtFirstNum);
+            this.Controls.Add(this.txtSecNum);
+            this.Controls.Add(this.txtSum);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.lblSecNum);
+            this.Controls.Add(this.lblFirstNum);
+            this.Name = "frmAddition";
+            this.Text = "Addition";
+            this.Load += new System.EventHandler(this.frmAddition_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirstNum;
+        private System.Windows.Forms.Label lblSecNum;
+        private System.Windows.Forms.Label lblSum;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox txtSum;
+        private System.Windows.Forms.TextBox txtSecNum;
+        private System.Windows.Forms.TextBox txtFirstNum;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AddNums
+{
+    public partial class frmAddition : Form
+    {
+        public frmAddition()
+        {
+            InitializeComponent();
+        }
+
+        private void lblFirstNum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+      
+        private void mnuFileExit_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSecNum_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmAddition_Load(object sender, EventArgs e)
+        {
+            //Set focus on the first number text field
+            txtFirstNum.Focus();
+            //Disable the sum textbox
+            txtSum.Enabled = false;
+        }
+
+        private void btnAdd_Click_1(object sender, EventArgs e)
+        {
+            //Check whether the first number text box is empty and display an appropriate message
+            if(txtFirstNum.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Please enter the first number to be added",
+                    "Missing first number", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Set Focus on the first number text box
+                txtFirstNum.Focus();
+                //Stop further method execution
+                return;
+            }
+
+            if (txtSecNum.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Please enter the second number to be added",
+                    "Missing second number", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //Set Focus on the second number text box
+                txtSecNum.Focus();
+                //Stop further method execution
+                return;
+            }
+
+            //Attempt to add the two numbers when the contain valid input
+            try
+            {
+                int num1 = int.Parse(txtFirstNum.Text);
+                int num2 = int.Parse(txtSecNum.Text);  //int num2 = Convert.ToInt30(txtSecNum.Text)
+                txtSum.Text = AddNums(num1, num2).ToString();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter a valid numeric value e.g. \'5\'",
+                    "Invalid", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("The error " + ex.Message + " occured!",
+                    "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// Method to  add two integers and return their sum as a long
+        /// </summary>
+        /// <param name="num1">First integer value to be added</param>
+        /// <param name="num2">Second integer value to be added</param>
+        /// <returns>Sum of the two integers as a long</returns>
+        private long AddNums (int num1, int num2)
+        {
+            return (num1 + num2);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            //Clear the first number textbox
+            txtFirstNum.Text = "";
+            //Clear the second number textbox
+            txtSecNum.Clear();
+            //Clear the sum number textbox
+            txtSum.Clear();
+            //Set Focus on the first number text box
+            txtFirstNum.Focus();
+        }
+    }
+}
